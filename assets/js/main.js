fa90196022778fbe37e8fb0cce508143b57c0f61
Yuthala <dinapolousova@gmail.com>
2024-09-10 12:24:12 +0300

    footer styles, scroll to to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,34 @@
+
 window.addEventListener('DOMContentLoaded', function() {
 	'use strict';
 
+
+/*переменная для scroll up*/
+let btn = document.getElementById('toTop');
+
+	//Уменьшаем padding Navbar при скролле вниз на 135px
 	window.addEventListener('scroll', function() {
 		document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 135);
+	});
+
+
+	/*Scroll up*/
+	window.addEventListener('scroll', function () {
+        // Если прокрутили дальше 400px, показываем кнопку
+        if (scrollY > 400) {
+            btn.classList.add('show');
+            // Иначе прячем
+        } else {
+            btn.classList.remove('show');
+        }
+    });
+
+    // При клике прокручиваем на самый верх
+	btn.addEventListener('click', function(click) {
+		click.preventDefault();
+		window.scrollTo({
+			top: 0, //scroll to the top of the page, edit if necessary
+			behavior: "smooth"
+		});
 	});
 }) 
