ec6983a3e249eec54c9c5dbb4ad168e63cb61607
Yuthala <dinapolousova@gmail.com>
2024-10-15 14:05:44 +0300

    sign up page

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,4 +68,25 @@ window.addEventListener('DOMContentLoaded', function() {
 			behavior: "smooth"
 		});
 	});
+
+	//Скрипт Bootstrap для валидации форм
+	// Example starter JavaScript for disabling form submissions if there are invalid fields
+	(() => {
+		'use strict'
+	
+		// Fetch all the forms we want to apply custom Bootstrap validation styles to
+		const forms = document.querySelectorAll('.needs-validation')
+	
+		// Loop over them and prevent submission
+		Array.from(forms).forEach(form => {
+		form.addEventListener('submit', event => {
+			if (!form.checkValidity()) {
+			event.preventDefault()
+			event.stopPropagation()
+			}
+	
+			form.classList.add('was-validated')
+		}, false)
+		})
+	})()
 });
